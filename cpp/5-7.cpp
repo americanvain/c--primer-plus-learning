797237a2a6e762d254812a8b3d11db4273b1fca0
americanvain <luckilyperson@163.com>
2021-10-11 20:16:53 +0800

    about 5-7.cpp

--- cpp/5-7.cpp
+++ cpp/5-7.cpp
@@ -5,7 +5,7 @@ using namespace std;
 struct car
 {
     string band;
-    int car_num;
+    int year_made;
 };
 
 int main(int argc, char const *argv[])
@@ -13,6 +13,19 @@ int main(int argc, char const *argv[])
     int num=0;
     cout<<"enter the num of the car\n";
     cin>>num;
-    cin>>num;
+    cin.get();
+    car *pt=new car [num];
+    for(int i=0;i<num;i++)
+    {
+        cout<<"enter the band";
+        getline(cin,pt[i].band);
+        cout<<"enter the year made";
+        cin>>pt[i].year_made;
+        cin.get();
+    }
+    for(int i=0;i<num;i++)
+    {
+        cout<<pt[i].year_made<<"   "<<pt[i].band<<endl;
+    }
     return 0;
 }
